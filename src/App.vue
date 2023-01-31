1f649e965cf5d60525bd7a2d6ef87156fbd9b015
Sebastian <ippo_sebas@hotmail.com>
2023-01-31 14:28:00 -0500

    "First commit"

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar color="blue" :collapse="!collapseOnScroll" :collapse-on-scroll="collapseOnScroll" absolute dark> 
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Marcador de asistencia GSI</v-toolbar-title>
+      <v-spacer></v-spacer>   
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </v-app-bar>
+        <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    collapseOnScroll: true,
+  }),
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,88 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <v-container class="contenedor">
+      <v-row class="contenido">
+        <v-col cols="12">
+          <v-time-picker v-model="tiempo" disabled use-seconds></v-time-picker>
+        </v-col>
+        <v-col cols="12" class="mt-5">
+          <div>
+            <v-alert
+              v-model="alert"
+              border="left"
+              close-text="Close Alert"
+              color="blue accent-4"
+              dark
+              dismissible
+            >
+              Se ha marcado correctamente la asistencia de este usuario :D
+            </v-alert>
+            <div class="text-center">
+              <button
+                type="button"
+                class="btn btn-primary boton"
+                :disabled="isDisabled"
+                v-if="!alert"
+                v-on:click="marcarAsistencia"
+              >
+                Marca asistencia
+              </button>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import { initializeApp } from "@firebase/app";
+//import { getFirestore, collection } from "@firebase/firestore";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+const firebaseConfig = {
+  apiKey: "AIzaSyAtIqDVWo7oKBw94iw9sZEGYGySH66pFi8",
+  authDomain: "gsi-vue-app.firebaseapp.com",
+  databaseURL: "https://gsi-vue-app-default-rtdb.firebaseio.com",
+  projectId: "gsi-vue-app",
+  storageBucket: "gsi-vue-app.appspot.com",
+  messagingSenderId: "901717682681",
+  appId: "1:901717682681:web:0f93492298a318e96e7528",
+  measurementId: "G-5GY7LC5C20",
+};
+
+const firebaseApp = initializeApp(firebaseConfig)
+console.log(firebaseApp)
+
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      tiempo: new Date(),
+      alert: false,
+      isDisabled: false,
+    };
+  },
+  methods: {
+    marcarAsistencia() {
+      this.alert = true;
+      this.isDisabled = true;
+    },
+  },
+};
+</script>
+<style>
+.contenedor {
+  margin: auto;
+  padding: 90px;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.contenido {
+  text-align: center;
+  justify-content: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.boton {
+  color: white;
 }
 </style>
